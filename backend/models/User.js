f45e77bd85e9735a06a76a848d8093bc4109b9e9
nirakar007 <neerakarsuwal106@gmail.com>
2025-07-19 14:18:26 +0545

    backend: creating user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,2 +1,36 @@
 // Mongoose schemas to define data structure
 // Schema for users (email, password, role, mfa_secret)
+
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["USER", "PRO", "ADMIN"],
+      default: "USER",
+    },
+    mfa_secret: {
+      type: String,
+    },
+    passwordLastChanged: {
+      type: Date,
+    },
+    passwordHistory: {
+      type: [String], // Array of previous password hashes
+    },
+  },
+  { timestamps: true }
+); // Adds createdAt and updatedAt fields
+
+module.exports = mongoose.model("User", UserSchema);
