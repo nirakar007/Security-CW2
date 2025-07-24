6c5d39811ea7a0183ed246746b897e32dbd0fb06
nirakar007 <neerakarsuwal106@gmail.com>
2025-07-25 03:49:34 +0545

    frontend: wrapped with auth provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,17 @@
 import { BrowserRouter as Router } from "react-router-dom";
 import AppRoutes from "./routes/AppRoutes";
-
-// Later, we can import things that appear on EVERY page, like a Navbar or Context
-// import Navbar from './components/layout/Navbar';
-// import { AuthProvider } from './context/AuthContext';
+import { AuthProvider } from "./components/context/AuthContext";
 
 function App() {
   return (
-    // The AuthProvider would wrap everything to provide global state
-    // <AuthProvider>
-    <Router>
-      {/* A global navbar would sit outside the AppRoutes component */}
-      {/* <Navbar /> */}
-
-      <main>
-        {/* AppRoutes now handles all the page switching logic */}
-        <AppRoutes />
-      </main>
-
-      {/* A global footer could go here */}
-    </Router>
-    // </AuthProvider>
+    <AuthProvider>
+      {" "}
+      <Router>
+        <main>
+          <AppRoutes />
+        </main>
+      </Router>
+    </AuthProvider>
   );
 }
 
